e0a0a776f7e460ad52551c4662cde1a0ddd2e127
zhangfan <cyouho@hotmail.co.jp>
2021-09-01 20:28:35 +0900

    a little fix

--- resources/views/index/index_contents.blade.php
+++ resources/views/index/index_contents.blade.php
@@ -1,94 +1,78 @@
 <section class="jumbotron text-center">
-        <div class="container">
-            <h1>以图搜图</h1>
-            <p class="lead text-muted">image search site.</p>
-        </div>
-    </section>
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                            xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                            role="img" aria-label="Placeholder: Thumbnail">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">Baidu</text>
-                        </svg>
-                        <div class="card-body">
-                            <p class="card-text">百度 以图搜图</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-primary"
-                                        onclick="window.open('/baidu')">点击开始</button>
-                                </div>
-                                <small class="text-muted">2020-11-16</small>
+    <div class="container">
+        <h1>以图搜图</h1>
+        <p class="lead text-muted">image search site.</p>
+    </div>
+</section>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Baidu</text>
+                    </svg>
+                    <div class="card-body">
+                        <p class="card-text">百度 以图搜图</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-primary" onclick="window.open('/baidu')">点击开始</button>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                            xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                            role="img" aria-label="Placeholder: Thumbnail">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">1688</text>
-                        </svg>
-                        <div class="card-body">
-                            <p class="card-text">1688 以图搜图</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-primary"
-                                    onclick="window.open('https://getbootstrap.com/docs/4.5/about/overview/')">点击开始</button>
-                                </div>
-                                <small class="text-muted">2020-11-16</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                            xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                            role="img" aria-label="Placeholder: Thumbnail">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">Alibaba（国际）</text>
-                        </svg>
-                        <div class="card-body">
-                            <p class="card-text">Alibaba（国际） 以图搜图</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-primary"
-                                    onclick="window.open('https://nginx.org/en/docs/')">点击开始</button>
-                                </div>
-                                <small class="text-muted">2020-11-16</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                            xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                            role="img" aria-label="Placeholder: Thumbnail">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">淘宝</text>
-                        </svg>
-                        <div class="card-body">
-                            <p class="card-text">淘宝 以图搜图</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-primary"
-                                    onclick="window.open('https://developer.mozilla.org/ja/docs/Web/API/Document')">点击开始</button>
-                                </div>
-                                <small class="text-muted">2020-11-19</small>
-                            </div>
+                            <small class="text-muted">2020-11-16</small>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">1688</text>
+                    </svg>
+                    <div class="card-body">
+                        <p class="card-text">1688 以图搜图</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-primary" onclick="window.open('/')">点击开始</button>
+                            </div>
+                            <small class="text-muted">2020-11-16</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Alibaba（国际）</text>
+                    </svg>
+                    <div class="card-body">
+                        <p class="card-text">Alibaba（国际） 以图搜图</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-primary" onclick="window.open('https://nginx.org/en/docs/')">点击开始</button>
+                            </div>
+                            <small class="text-muted">2020-11-16</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">淘宝</text>
+                    </svg>
+                    <div class="card-body">
+                        <p class="card-text">淘宝 以图搜图</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-primary" onclick="window.open('https://developer.mozilla.org/ja/docs/Web/API/Document')">点击开始</button>
+                            </div>
+                            <small class="text-muted">2020-11-19</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
