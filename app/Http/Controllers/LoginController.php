fb80476459d27549d5f90e427d10d82a207dc9ba
zhangfan <cyouho@hotmail.co.jp>
2021-09-04 10:33:39 +0900

    add login controller for master and remake register controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RegisterController extends Controller
+class LoginController extends Controller
 {
     //
 }
