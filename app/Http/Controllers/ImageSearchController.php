2553eb54f004f5c1e4f8a57442d2342d188ea33b
zhangfan <cyouho@hotmail.co.jp>
2021-09-06 11:10:24 +0900

    add commit image search and its function

--- app/Http/Controllers/ImageSearchController.php
+++ app/Http/Controllers/ImageSearchController.php
@@ -6,25 +6,47 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use View;
 
+/**
+ * Main function for image search.
+ */
 class ImageSearchController extends Controller
 {
+    /**
+     * Show image search home page for baidu.
+     */
     public function baidu()
     {
         return view('imgsearch.baidu');
     }
 
+    /**
+     * Show image search home page for 1688.
+     */
     public function onesix()
     {
         return view('imgsearch.1688');
     }
 
+    /**
+     * Show image search home page for alibaba.
+     */
     public function alibaba()
     {
         return view('imgsearch.alibaba');
     }
 
+    /**
+     * Show image search home page for taobao.
+     */
     public function taobao()
     {
         return view('imgsearch.taobao');
+    }
+
+    /**
+     * Image search function for baidu.
+     */
+    public function baiduImageSearch()
+    {
     }
 }
