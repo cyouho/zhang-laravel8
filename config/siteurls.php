faa76fb1a47efb7e06eeac6f3b462bb926653488
zhangfan <cyouho@hotmail.co.jp>
2021-09-10 01:22:51 +0900

    add onesix function for timestamp

--- config/siteurls.php
+++ config/siteurls.php
@@ -1,21 +1,20 @@
 <?php
 return [
     'imageSearch' => [
-        'homePageHost' => [
-            'baidu'  => 'https://graph.baidu.com/',
-            'onesix' => 'https://www.1688.com/'
+        'baidu' => [
+            'homePageHost' => 'https://graph.baidu.com/',
+            'postHost'     => 'https://graph.baidu.com/upload',
+            'resualtHost'  => 'https://graph.baidu.com/s?sign='
         ],
-        'postHost' => [
-            'baidu'  => 'https://graph.baidu.com/upload',
-            'onesix' => [
+        'onesix' => [
+            'homePageHost' => 'https://www.1688.com/',
+            'postHost'     => [
                 'timestamp' => 'https://open-s.1688.com/openservice/.htm?',
                 'sign'      => 'https://open-s.1688.com/openservice/ossDataService',
                 'img'       => 'https://cbusearch.oss-cn-shanghai.aliyuncs.com/'
-            ]
-        ],
-        'resualtHost' => [
-            'baidu'  => 'https://graph.baidu.com/s?sign=',
-            'onesix' => 'https://s.1688.com/youyuan/index.htm?tab=imageSearch&imageType=oss&imageAddress='
+            ],
+            'resualtHost'  => 'https://s.1688.com/youyuan/index.htm?tab=imageSearch&imageType=oss&imageAddress=',
+            'serviceIds'   => 'cbu.searchweb.config.system.currenttime'
         ]
     ]
 ];
