25bde306721af4a2e14e5d78d82c27c6527df54a
zhangfan <cyouho@hotmail.co.jp>
2021-09-11 10:44:33 +0800

    test

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use View;
 use DB;
 
 /**
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User\User;
 
 /**
  * Register Controller
