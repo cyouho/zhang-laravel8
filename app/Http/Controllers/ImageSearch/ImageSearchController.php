0d758a296561da87ed71b49fcef22bc271cf92eb
zhangfan <cyouho@hotmail.co.jp>
2021-10-03 10:38:06 +0800

    add Util class

--- app/Http/Controllers/ImageSearch/ImageSearchController.php
+++ app/Http/Controllers/ImageSearch/ImageSearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\ImageSearch;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Util;
 
 class ImageSearchController extends Controller
 {
--- app/Http/Controllers/ImageSearch/Util.php
+++ app/Http/Controllers/ImageSearch/Util.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\ImageSearch;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class Util extends Controller
 {
-    //
+    public static function executePost($postData = [], $formData = []) {
+        $client = new Client();
+        $response = $client->request('POST', $postData['postHost'], [
+            'form_params'  => $formData
+        ]);
+        return $response;
+    }
 }
