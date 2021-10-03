1e0819174a92142704a510fe05b2fd8c7057d20e
zhangfan <cyouho@hotmail.co.jp>
2021-10-03 10:27:49 +0800

    remake image search floder

---
+++ app/Http/Controllers/ImageSearch/ImageSearchController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ImageSearch;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ImageSearchController extends Controller
+{
+    //
+}
