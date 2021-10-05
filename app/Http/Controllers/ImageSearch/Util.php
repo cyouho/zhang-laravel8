cb60fcaf8d1adbf5185e84602c32094f235c0f7c
zhangfan <cyouho@hotmail.co.jp>
2021-10-05 08:55:36 +0800

    add onesix image search function all

--- app/Http/Controllers/ImageSearch/Util.php
+++ app/Http/Controllers/ImageSearch/Util.php
@@ -8,11 +8,31 @@ use GuzzleHttp\Client;
 
 class Util extends Controller
 {
-    public static function executePost($postData = [], $formData = []) {
+    public static function executePost($postData = [], $formData = [])
+    {
         $client = new Client();
         $response = $client->request('POST', $postData['postHost'], [
             'form_params'  => $formData
         ]);
         return $response;
+    }
+
+    public static function getFileContents($image)
+    {
+        $file = file_get_contents($image);
+        return $file;
+    }
+
+    public static function getRandomString($len, $chars = '')
+    {
+        if (empty($chars)) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        }
+
+        for ($i = 0, $str = '', $lc = strlen($chars) - 1; $i < $len; $i++) {
+            $str .= $chars[mt_rand(0, $lc)];
+        }
+
+        return $str;
     }
 }
