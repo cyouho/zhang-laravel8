2cc2d6d4847ea1669398ab8652dc3dddc984a7e2
zhangfan <cyouho@hotmail.co.jp>
2021-11-15 08:20:51 +0800

    add function doc

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
+/**
+ * 所有模型 Models 所使用的静态方法类
+ */
 class Utils
 {
+    /**
+     * 从 email 中获取 name
+     */
     public static function getNameFromEmail($email)
     {
         return substr($email, 0, strripos($email, "@"));
     }
 
+    /**
+     * 使用 md5 来生成所需的 session
+     */
     public static function getSessionRandomMD5()
     {
         return md5(time());
